1a906d0b26c9fcbe0c6837eb96bdb20704ebc8f4
Colin <colin.diesh@gmail.com>
2019-04-15 17:17:56 -0400

    Make clicking on a peak give feature details

--- js/View/Track/Base.js
+++ js/View/Track/Base.js
@@ -2,19 +2,23 @@ define([
     'dojo/_base/declare',
     'dojo/_base/array',
     'dojo/_base/lang',
+    'dojo/on',
+    'dijit/Dialog',
     'JBrowse/Util',
     'JBrowse/Store/SeqFeature/BigBed',
+    'JBrowse/View/Track/_FeatureDetailMixin',
 ],
 function (
     declare,
     array,
     lang,
+    on,
+    Dialog,
     Util,
     BigBed,
+    FeatureDetailMixin
 ) {
-    return declare(null, {
-
-
+    return declare(FeatureDetailMixin, {
         constructor: function (args) {
             const ret = Object.assign({},this.config.bigbed,args)
             this.highlightStore = new BigBed(ret)
@@ -36,28 +40,41 @@ function (
                 feature => {
                     const s = block.bpToX(Math.max(feature.get('start')-this.config.broaden,block.startBase))
                     const e = block.bpToX(Math.min(feature.get('end')+this.config.broaden,block.endBase))
-                    const ret = dojo.create('div',
-                        {
-                            style: {
-                                left: `${s}px`,
-                                width: `${e-s}px`,
-                                height: canvas.style.height,
-                                top: 0,
-                                position: 'absolute',
-                                backgroundColor: this.config.highlightColor
-                            }
-                        }, block.domNode)
-                    const indicator = dojo.create('div',
-                        {
-                            style: {
-                                left: `${s}px`,
-                                width: `${e-s}px`,
-                                height: `${this.config.indicatorHeight}px`,
-                                top: canvas.style.height,
-                                position: 'absolute',
-                                backgroundColor: this.config.indicatorColor
-                            }
-                        }, block.domNode)
+                    const ret = dojo.create('div', {
+                        style: {
+                            left: `${s}px`,
+                            width: `${e-s}px`,
+                            height: canvas.style.height,
+                            top: 0,
+                            zIndex: 10000,
+                            position: 'absolute',
+                            backgroundColor: this.config.highlightColor
+                        }
+                    }, block.domNode)
+                    const indicator = dojo.create('div', {
+                        style: {
+                            left: `${s}px`,
+                            width: `${e-s}px`,
+                            height: `${this.config.indicatorHeight}px`,
+                            zIndex: 10000,
+                            top: canvas.style.height,
+                            position: 'absolute',
+                            backgroundColor: this.config.indicatorColor
+                        }
+                    }, block.domNode)
+                    console.log('herere')
+                    on(indicator, 'click',
+                      () => {
+                          console.log('herere')
+                          new Dialog({ content: this.defaultFeatureDetail(this, feature, null, null, null) }).show()
+                      }
+                    )
+                    on(ret, 'click',
+                        () => {
+                          console.log('herere2')
+                          new Dialog({ content: this.defaultFeatureDetail(this, feature, null, null, null) }).show()
+                      }
+                    )
                 },
                 () => { },
                 error => { console.error(error) }
